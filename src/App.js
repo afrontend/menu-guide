2569cd7f6667596b04fae570fff049dc6bf263e7
afrontend <frontend.wordpress@gmail.com>
2019-11-24 20:03:13 +0900

    Add menus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <nav class="panel">
+        <p class="panel-tabs">
+        <a href="/" class="is-active">커피</a>
+        <a href="/">점심</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="panel-block">
+          <input class="input" type="text" placeholder="?" /> &nbsp;
+          <div class="button is-primary is-fullwidth"><span class="title is-6">이거요</span></div>
+        </div>
+        <div class="panel-block">
+          <div class="button is-success is-fullwidth"><span class="title is-6">핫아메리카노</span></div>&nbsp;
+          <div class="button is-outlined is-1"><span class="title is-6">3</span></div>&nbsp;
+          <div class="button is-danger is-1"><span class="title is-6">X</span></div>
+        </div>
+        <div class="panel-block">
+          <div class="button is-success is-fullwidth"><span class="title is-6">아이스아메리카노</span></div>&nbsp;
+          <div class="button is-outlined is-1"><span class="title is-6">3</span></div>&nbsp;
+          <div class="button is-danger is-1"><span class="title is-6">X</span></div>
+        </div>
+      </nav>
     </div>
   );
 }
