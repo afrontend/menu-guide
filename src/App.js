0d8995347d95145ee47627f0e24fafa8ad53addb
afrontend <frontend.wordpress@gmail.com>
2019-12-01 22:45:06 +0900

    Use functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import './App.css';
 
+const menuList = [
+  'apple',
+  'banana',
+  'melon',
+];
+
+function menu(menuName, index) {
+  return (
+    <div key={ index } className="panel-block">
+      <div className="button is-success is-fullwidth"><span className="title is-6">{ menuName }</span></div>&nbsp;
+      <div className="button is-outlined is-1"><span className="title is-6">3</span></div>&nbsp;
+      <div className="button is-danger is-1"><span className="title is-6">X</span></div>
+    </div>
+  );
+}
+
+function addMenu() {
+  menuList.push("jfdjf");
+  console.log(menuList);
+}
+
+function title() {
+  return  (
+    <div className="panel-block">
+      <input className="input" type="text" placeholder="?" /> &nbsp;
+      <div className="button is-primary is-fullwidth" onClick={ addMenu }><span className="title is-6">이거요</span></div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <nav class="panel">
-        <p class="panel-tabs">
-        <a href="/" class="is-active">커피</a>
-        <a href="/">점심</a>
+      <nav className="panel">
+        <p className="panel-tabs">
+          <a href="/" className="is-active">커피</a>
+          <a href="/">점심</a>
         </p>
-        <div class="panel-block">
-          <input class="input" type="text" placeholder="?" /> &nbsp;
-          <div class="button is-primary is-fullwidth"><span class="title is-6">이거요</span></div>
-        </div>
-        <div class="panel-block">
-          <div class="button is-success is-fullwidth"><span class="title is-6">핫아메리카노</span></div>&nbsp;
-          <div class="button is-outlined is-1"><span class="title is-6">3</span></div>&nbsp;
-          <div class="button is-danger is-1"><span class="title is-6">X</span></div>
-        </div>
-        <div class="panel-block">
-          <div class="button is-success is-fullwidth"><span class="title is-6">아이스아메리카노</span></div>&nbsp;
-          <div class="button is-outlined is-1"><span class="title is-6">3</span></div>&nbsp;
-          <div class="button is-danger is-1"><span class="title is-6">X</span></div>
-        </div>
+        { title() }
+        {
+          menuList.map((menuName, index) => {
+            return menu(menuName, index);
+          })
+        }
       </nav>
     </div>
   );
